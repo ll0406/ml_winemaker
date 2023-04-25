02b4eeafd117e7d90a6d4cc9c00b1485e10347bf
Li Liu <liliu@Lis-MacBook-Pro.local>
2023-04-24 23:36:40 -0400

    update

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+import streamlit as st
+
 
 def detector_value(prob_storm, model_sense, model_spec, har_payout, nhar_payout_storm, nhar_payout_nstorm):
     p_dns = (model_spec * (1 - prob_storm)) + ((1 - model_sense) * prob_storm)
@@ -27,8 +29,38 @@ def detector_value(prob_storm, model_sense, model_spec, har_payout, nhar_payout_
 
     # print(f"payout_d: {payout_d}")
 
-    return val_d
+    return val_d, payout_d
 
-if __name__ == '__main__':
-    dv = detector_value(.5, 0.39, 0.69, 80000.0, 59000.0, 95750.0)
-    print(f"The detector value is {dv}")
+def calculate_nhar_payout_storm(botrytis_prob):
+    return (1-botrytis_prob) * 35000 + botrytis_prob * 275000
+
+def calculate_nhar_payout_nstorm(no_prob, typical_prob, high_prob):
+    return no_prob * 80000 + typical_prob * 117500 + high_prob * 125000
+
+
+st.title('Winemaker Dilemma III')
+
+botrytis = st.number_input('Chance of botrytis')
+no_sugar = st.number_input('Chance of no sugar increase')
+typical_sugar = st.number_input('Chance of typical sugar increase')
+high_sugar = st.number_input('Chance of high sugar increase')
+
+
+# Calculate payout
+nhar_payout_nstorm = calculate_nhar_payout_nstorm(no_sugar, typical_sugar, high_sugar)
+nhar_payout_storm = calculate_nhar_payout_storm(botrytis)
+dv, ev = detector_value(.5, 0.39, 0.69, 80000.0, nhar_payout_storm, nhar_payout_nstorm)
+
+st.write('The E-Value of Not Buying Detector is: $', 80000)
+st.write('The E-Value of Buying Detector is: $', ev)
+
+rec = "Buy Detector" if dv > 0 else "Do Not Buy Detector"
+
+st.write('Recommendation: ', rec)
+
+
+    # session = True
+    # while session:
+    #     print("Welcome to E-Value Calculator")
+    # dv = detector_value(.5, 0.39, 0.69, 80000.0, 59000.0, 95750.0)
+    # print(f"The detector value is {dv}")
